b7acc61994794db31e57e9df4a4af6b5ffbc24ac
whitebbit <whitebbit.entertainment@gmail.com>
2023-09-12 19:43:37 +0300

    add parse tests

--- main.py
+++ main.py
@@ -21,3 +21,10 @@ def parse(query: str) -> dict:
     return parsed
 
 
+if __name__ == '__main__':
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('http://example.com/') == {}
+    assert parse('http://example.com/?') == {}
+    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+
